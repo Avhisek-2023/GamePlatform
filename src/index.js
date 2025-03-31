4bbadb902807c5220a421ec51e43c62062927101
Priyanshu Chourasia <priyanshuchourasia916@gmail.com>
2025-03-31 21:59:00 +0530

    Docker compose updated, Commented codes are removed package.json update with cors and pm2 package

--- src/index.js
+++ src/index.js
@@ -6,10 +6,18 @@ import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import swaggerUi from "swagger-ui-express";
 import swaggerDocument from "./swagger-output.json" with { type: "json" };
+import cors from "cors";
+import User from "./models/users.js";
 
 dotenv.config();
 
 const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
@@ -23,8 +31,6 @@ app.get('/',(req,res)=>{
   })
 });
 
-app.use(express.json());
-app.use(cookieParser());
 
 app.use("/api", authRoutes);
 app.use("/api", userRoutes);
