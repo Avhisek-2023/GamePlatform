d295b195dfebe5fbe8ab983e32dc7d47be669440
Priyanshu Chourasia <priyanshuchourasia916@gmail.com>
2025-04-25 21:38:25 +0530

    Upload zip error solve

--- src/lib/uploadZip.js
+++ src/lib/uploadZip.js
@@ -1,48 +1,25 @@
 import unzipper from "unzipper";
 import fs from "fs";
 import path from "path";
-
 export const upload = async (file, projectId, projectName, res) => {
   const zipFilePath = file.path;
-  const fileNameWithoutExt = path.basename(file.originalname, path.extname(file.originalname));
-  const extractPath = path.join("/app/games", fileNameWithoutExt);
-  console.log(file.path, "file path");
   const extractPath = path.join("/app/games", `${projectId}`);
 
   try {
     fs.mkdirSync(extractPath, { recursive: true });
-
     fs.createReadStream(zipFilePath)
-      .pipe(unzipper.Parse())
-      .on("entry", (entry) => {
-        let relativePath = entry.path;
-
-  
-        if (relativePath.startsWith("dist/")) {
-          relativePath = relativePath.replace(/^dist\//, "");
-        }
-
-        const targetPath = path.join(extractPath, relativePath);
-
-        if (entry.type === "File") {
-          fs.mkdirSync(path.dirname(targetPath), { recursive: true });
-          entry.pipe(fs.createWriteStream(targetPath));
-        } else {
-          entry.autodrain();
-        }
-      })
-      .on("close", () => {
+      .pipe(unzipper.Extract({ path: extractPath }))
+      .on("finish", () => {
         fs.unlink(zipFilePath, (err) => {
-          if (err) console.error("Error deleting zip:", err);
+          if (err) {
+            console.error(err);
+          }
         });
       })
       .on("error", (err) => {
-        console.error("Unzip error:", err);
-        res.status(500).json({ message: err.message });
+        res.status(500).json({ message: err });
       });
-
   } catch (err) {
-    console.error("Upload error:", err);
-    res.status(500).json({ message: err.message });
+    res.status(500).json({ message: err });
   }
 };
